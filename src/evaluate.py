f70dfbe2e87d88f2117e985eca05bd3569a54e6d
samp603 <samuelparis603@gmail.com>
2025-07-15 13:11:51 -0400

    Code

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,1 +1,5 @@
+# Placeholder for evaluation/visualization
 
+# TODO: Add model comparison plots (bar chart of RMSE)
+# TODO: Add price prediction vs. actual scatterplot
+# TODO: Show residuals histogram
